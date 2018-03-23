a6ecdf010cffbfaf6a0dde319b31986af360c319
Zach Studer <zstuder25@gmaill.com>
2018-03-23 18:28:23 +0000

    Improve the readme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "¡hola, mundo!"
+    render html: "goodbye, world!"
   end
 end
